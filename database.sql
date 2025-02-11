c8a3655eac5229c382d5b6dad5c2504f423de4ac
Tom <157160253+txlney@users.noreply.github.com>
2025-02-11 21:24:02 +0000

    added more entities

--- database.sql
+++ database.sql
@@ -1,16 +1,27 @@
--- ERD: https://www.edrawmax.com/online/share.html?code=9b7de5cee88011ef9c1f0a54be41f961
--- Data Dictionary: https://docs.google.com/document/d/1FZFvx8Ackj2w9GUdl1m55n319Ih34K4-ekknqC8ot4k/edit?usp=sharing
-
 /*
-entities:
 
-user
-preferences
-user-preferences
+Links to database logical resources:
 
-saved-recipes
+- ERD: https://www.edrawmax.com/online/share.html?code=9b7de5cee88011ef9c1f0a54be41f961
+- Data Dictionary: https://docs.google.com/document/d/1FZFvx8Ackj2w9GUdl1m55n319Ih34K4-ekknqC8ot4k/edit?usp=sharing
 
-recipe
-ingredient
-recipe-ingredient
+
+Entities:
+
+- users
+- preferences
+- user-preferences
+- friends
+
+- favourites (recipes)
+- ratings
+
+- recipes
+- ingredients
+- recipe-ingredients
+- allergies
+- ingredient-allergens
+- categories
+- recipe-categories
+
 */
