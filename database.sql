ba1b2aa23fbbd0460292e0d9654b71cf5c3af7b0
txlney <thomas.olney1@gmail.com>
2025-02-11 14:05:13 +0000

    Initial database entities

---
+++ database.sql
@@ -0,0 +1,15 @@
+-- ERD: https://www.edrawmax.com/online/share.html?code=9b7de5cee88011ef9c1f0a54be41f961
+
+/*
+entities:
+
+user
+preferences
+user-preferences
+
+saved-recipes
+
+recipe
+ingredient
+recipe-ingredient
+*/
