2c7218459e0ed9a007ff3e19daceb0825eaa5fb5
Tom <157160253+txlney@users.noreply.github.com>
2025-02-11 20:55:07 +0000

    added links to ERD and Data Dictionary

--- database.sql
+++ database.sql
@@ -1,4 +1,5 @@
 -- ERD: https://www.edrawmax.com/online/share.html?code=9b7de5cee88011ef9c1f0a54be41f961
+-- Data Dictionary: https://docs.google.com/document/d/1FZFvx8Ackj2w9GUdl1m55n319Ih34K4-ekknqC8ot4k/edit?usp=sharing
 
 /*
 entities:
