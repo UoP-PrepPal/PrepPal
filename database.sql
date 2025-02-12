0f2f75f01b2ff30a3f1bfaa4a519c1b38ceb6b6f
Jonathan Parker <jonathanjparker2@gmail.com>
2025-02-12 13:54:18 +0000

    added sql for friends table

--- database.sql
+++ database.sql
@@ -26,6 +26,8 @@ Entities:
 
 */
 
+CREATE TYPE statuses AS ENUM ('Online', 'Offline');
+
 CREATE TABLE Users (
     user_id SERIAL, 
     username varchar(20) NOT NULL UNIQUE, 
@@ -36,4 +38,17 @@ CREATE TABLE Users (
     created_at DATE DEFAULT current_date,
     profile_picture VARCHAR(50), 
     PRIMARY KEY (user_id)
-)
+);
+
+CREATE TABLE Friends (
+    friendship_id SERIAL,
+    user_id int NOT NULL,
+    friend_id int NOT NULL,
+    friend_status statuses NOT NULL,
+    requested_at DATE DEFAULT current_date,
+    accepted_at DATE,
+    blocked_at DATE,
+    PRIMARY KEY (friendship_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (friend_id) REFERENCES Users(user_id)
+);
