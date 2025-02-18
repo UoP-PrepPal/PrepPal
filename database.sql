f9d88021a839b349008076b95846b3bad65a7e41
AzizulTarafder <up2123872@myport.ac.uk>
2025-02-18 12:46:03 +0000

    hi

--- database.sql
+++ database.sql
@@ -26,6 +26,46 @@ Entities:
 
 */
 
+CREATE TYPE statuses AS ENUM ('Online', 'Offline');
+
+CREATE TABLE Users (
+    user_id SERIAL, 
+    username varchar(20) NOT NULL UNIQUE, 
+    email varchar(100) NOT NULL UNIQUE,
+    first_name varchar(20),
+    last_name varchar(20),
+    -- password_hash NOT NULL,
+    created_at DATE DEFAULT current_date,
+    profile_picture VARCHAR(50), 
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Friends (
+    friendship_id SERIAL,
+    user_id int NOT NULL,
+    friend_id int NOT NULL,
+    friend_status statuses NOT NULL,
+    requested_at DATE DEFAULT current_date,
+    accepted_at DATE,
+    blocked_at DATE,
+    PRIMARY KEY (friendship_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (friend_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Preferences (
+    preference_id SERIAL,
+    name VARCHAR(20) NOT NULL UNQIUE,
+    description VARCHAR(255) NOT NULL,
+    PRIMARY KEY (preference_id)
+);
+
+CREATE TABLE user_preferences (
+    user_id int NOT NULL REFERENCES Users(user_id),
+    preference_id int NOT NULL REFERENCES Preferences(preference_id),
+    PRIMARY KEY (user_id, preference_id)
+);
+
 CREATE TYPE statuses AS ENUM ('Requested', 'Accepted', 'Blocked');
 
 CREATE TABLE users (
@@ -64,13 +104,6 @@ CREATE TABLE recipes (
     date_added DATE NOT NULL DEFAULT current_date
 );
 
-CREATE TABLE preferences (
-    preference_id SERIAL,
-    name VARCHAR(20) NOT NULL UNIQUE,
-    description VARCHAR(255) NOT NULL,
-    PRIMARY KEY (preference_id)
-);
-
 CREATE TABLE ratings (
     rating_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL REFERENCES users(user_id),
@@ -105,6 +138,13 @@ CREATE TABLE recipe_ingredients (
     PRIMARY KEY (recipe_id, ingredient_id),
     unit VARCHAR(10) NOT NULL
 );
+
+CREATE TABLE favourites (
+    user_id int NOT NULL REFERENCES Users(user_id),
+    recipe_id int NOT NULL REFERENCES recipes(recipe_id),
+    PRIMARY KEY (user_id, recipe_id),
+    date_added DATE DEFAULT CURRENT_DATE
+)
 
 CREATE TABLE allergies (
     allergy_id SERIAL PRIMARY KEY,
