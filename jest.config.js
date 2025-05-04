407dd22377650f39ce5d514ca65c73c65e1c283e
Jonathan Parker <jonathanjparker2@gmail.com>
2025-05-04 19:26:35 +0100

    updated configurations to allow test imports

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,8 @@
-testEnvironment: "jsdom"
+// testEnvironment: "jsdom"
+
+export default {
+    transform: {
+      "^.+\\.js$": "babel-jest", // Use Babel to transpile JavaScript files
+    },
+    testEnvironment: "node", // Use Node.js environment for testing
+  };
