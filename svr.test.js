a2a27579bc4c2d45b9985b71e1bc28cdb9a59ce7
Jonathan Parker <jonathanjparker2@gmail.com>
2025-05-04 19:06:04 +0100

    added import statements

--- svr.test.js
+++ svr.test.js
@@ -1,3 +1,6 @@
+import request from "supertest";
+import svr from "./svr.js";
+
 // Template for a unit test
 describe('Application functionality', () => {
   test('should return true for valid input', () => {
