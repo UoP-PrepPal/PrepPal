db54654444ab7cef77b86d308c4b6f1e4a4b35d6
Jonathan Parker <jonathanjparker2@gmail.com>
2025-05-06 11:43:35 +0100

    added test suite for testing dashboard access

--- svr.test.js
+++ svr.test.js
@@ -1,14 +1,14 @@
 import request from "supertest";
 import app from "./svr.js";
 
-// Template for a unit test
-// describe('Application functionality', () => {
-//  test('should return true for valid input', () => {
-//    expect(true).toBe(true);
-//  });
-// });
+/*
+npm installs I used:
+  npm install --save-dev jest @jest-environment-jsdom
+  npm install --save-dev jest babel-jest @babel/preset-env
+  npm install --save-dev supertest
+*/ 
 
-describe("Entering Details", () => {
+describe("Entering Details", () => {  // Test suite for entering user details for sign up / sign in
   test("signup should return error status 400 for missing fields", async () => {
     const res = await request(app)
       .post("/signup")
@@ -33,3 +33,26 @@ describe("Entering Details", () => {
     expect(res.body.error).toBe("Invalid credentials");
   });
 })
+
+
+describe("Dashboard Access", () => {
+  test("dashboard should return error status 401 if user is not logged in", async () => {
+    const res = await request(app).get("/dashboard");
+    expect(res.statusCode).toBe(401);
+    expect(res.body.error).toBe("You must be logged in to view the dashboard");
+  });
+
+  test("dashboard should return success status 200 if user is logged in", async () => {
+    const agent = request.agent(app); 
+
+    // Simulating a user sign in
+    await agent.post("/signIn").send({
+      username: "testing",
+      email: "testing@testing.com",
+    });
+
+    const res = await agent.get("/dashboard");
+    expect(res.statusCode).toBe(200);
+    expect(res.headers["content-type"]).toContain("text/html");
+  });
+});
